0b7514adef60a1f27a2b2bd7a8e410736bd45598
Marat <tagirovmm4951@gmail.com>
2023-07-29 21:45:17 +0500

    whyyyyyy? Why backdground doesn't load?

--- zcopper/blog/models.py
+++ zcopper/blog/models.py
@@ -5,7 +5,7 @@ class StandartPost(models.Model):
     title = models.CharField(max_length=255)
     post_author = models.TextField(blank=True)
     post_header_image = models.ImageField(upload_to='photos/%d/%m/%Y')
-    post_text = models.TextField
+    post_text = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
--- zcopper/blog/views.py
+++ zcopper/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-
+from .models import *
 
 def index(request):
-    return render(request, 'blog/base.html',
-                  {'title': 'Привет, мой хороший(ая)❤️! Ты на главной странице нашего блога.'})
+    posts = StandartPost.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def about(request):
